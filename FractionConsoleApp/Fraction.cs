5468abcc3c1ba589486c5d95a0815b36c0b172f3
Евгений Евгений <vishnyakov0210@gmail.com>
2024-06-11 21:09:05 +0300

    Add project files.

---
+++ FractionConsoleApp/Fraction.cs
@@ -0,0 +1,108 @@
+﻿namespace FractionConsoleApp;
+
+public class Fraction
+{
+    /// <summary>
+    /// Числитель
+    /// </summary>
+    public int Numerator;
+    /// <summary>
+    /// Знаменатель
+    /// </summary>
+    public int Denominator;
+
+    public Fraction(int number)
+    {
+        Numerator = number;
+        Denominator = 1;
+    }
+    public Fraction(int numerator, int denominator)
+    {
+          Numerator = numerator;
+          Denominator = denominator;              
+    }    
+    public void Print()
+    {
+        GetReduceFraction();
+        if (Denominator == 1)
+        {
+            Console.WriteLine(Numerator);
+        }
+        else if (Math.Abs(Numerator) > Denominator && Numerator < 0)
+        {
+            Numerator *= -1;
+            int wholeNumber = GetWholeNumber();
+            Console.WriteLine($"-{wholeNumber}_{Numerator}/{Denominator}");
+        }
+        else if (Numerator > Denominator)
+        {
+            int wholeNumber = GetWholeNumber();
+            Console.WriteLine($"{wholeNumber}_{Numerator}/{Denominator}");
+        }
+        else
+        {
+            Console.WriteLine($"{Numerator}/{Denominator}");
+        }
+    }
+    private void GetReduceFraction()
+    {
+        for (int i = 2; i <= Math.Min(Numerator, Denominator); i++)
+        {
+            while (Numerator % i == 0 && Denominator % i == 0)
+            {
+                Numerator /= i;
+                Denominator /= i;
+            }
+        }
+    }
+    private int GetWholeNumber()
+    {
+        int wholeNumber = Numerator / Denominator;
+        GetNumerator(wholeNumber);
+        return wholeNumber;
+    }
+    private void GetNumerator(int wholeNumber)
+    {
+        Numerator -= wholeNumber * Denominator;
+    }
+    public Fraction Sum(Fraction otherFraction)
+    {
+        var commonDenominator = Denominator * otherFraction.Denominator;
+        var resultNumenator = Numerator * otherFraction.Denominator + otherFraction.Numerator * Denominator;
+        return new Fraction(resultNumenator, commonDenominator);
+    }
+    public Fraction Difference(Fraction otherFraction)
+    {
+        var commonDenominator = Denominator * otherFraction.Denominator;
+        var resultNumenator = Numerator * otherFraction.Denominator - otherFraction.Numerator * Denominator;
+        return new Fraction(resultNumenator, commonDenominator);
+    }
+    public Fraction Multiply(Fraction otherFraction)
+    {
+        return new Fraction(Numerator * otherFraction.Numerator, Denominator * otherFraction.Denominator);
+    }
+    public Fraction Devide(Fraction otherFraction)
+    {
+        return new Fraction(Numerator * otherFraction.Denominator, Denominator * otherFraction.Numerator);
+    }
+    public Fraction Sum(int number)
+    {
+        Fraction otherFraction = new Fraction(number);
+        return Sum(otherFraction);
+    }
+    public Fraction Difference(int number)
+    {
+        Fraction otherFraction = new Fraction(number);
+        return Difference(otherFraction);
+    }
+    public Fraction Multiply(int number)
+    {
+        Fraction otherFraction = new Fraction(number);
+        return Multiply(otherFraction);
+    }
+    public Fraction Devide(int number)
+    {
+        Fraction otherFraction = new Fraction(number);
+        return Devide(otherFraction);
+    }
+}
---
+++ FractionConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace FractionConsoleApp;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Fraction fraction1 = new Fraction(1, 2);
+        Fraction fraction2 = new Fraction(2, 3);
+        fraction1.Print();
+        fraction2.Print();
+        Fraction resultSum = fraction1.Sum(fraction2);
+        resultSum.Print();
+        Fraction resultDiffernce = fraction1.Difference(fraction2);
+        resultDiffernce.Print();
+        Fraction resultMultiply = fraction1.Multiply(fraction2);
+        resultMultiply.Print();
+        Fraction resultDevide = fraction1.Devide(fraction2);
+        resultDevide.Print();
+        Console.WriteLine("__________________________");
+        Fraction resultSumNumber = fraction1.Sum(4);
+        resultSumNumber.Print();
+        Fraction resultDifferenceNumber = fraction1.Difference(4);
+        resultDifferenceNumber.Print();
+        Fraction resultMultiplyNumber = fraction1.Multiply(4);
+        resultMultiplyNumber.Print();
+        Fraction resultDevideNumber = fraction1.Devide(2);
+        resultDevideNumber.Print();
+    }
+}
